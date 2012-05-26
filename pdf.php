208bfe975707bec55371616e60e6a5fbe942c9c8
khalifa-chokri <khalifa.chokri@gmail.com>
2012-05-26 17:17:11 +0200

    Update pdf.php

--- pdf.php
+++ pdf.php
@@ -1,6 +1,13 @@
 <?php
 require('pdf/fpdf.php');
-
+/*
+*
+* Class créé par chokri khalifa
+* 
+*
+*
+*
+*/
 class PDF extends FPDF{
 var $B;
 var $I;
@@ -18,14 +25,14 @@ function PDF($orientation='P', $unit='mm', $size='A4'){
 }
     
 function Header(){
-    // Logo
+    // Logo a inséré 
     $this->Image('image.jpg',10,6,30);
     // Police Arial gras 15
     $this->SetFont('Arial','B',15);
     // Décalage à droite
     $this->Cell(80);
-    // Titre
-    $this->Cell(30,10,'Facture',0,0,'C');
+    // Titre du document
+    $this->Cell(30,10,'Titre du document',0,0,'C');
     // Saut de ligne
     $this->Ln(20);
 }
@@ -44,29 +51,23 @@ function Footer(){
         // Parseur HTML
         $html = str_replace("\n",' ',$html);
         $a = preg_split('/<(.*)>/U',$html,-1,PREG_SPLIT_DELIM_CAPTURE);
-        foreach($a as $i=>$e)
-        {
-            if($i%2==0)
-            {
+        foreach($a as $i=>$e){
+            if($i%2==0){
                 // Texte
                 if($this->HREF)
                     $this->PutLink($this->HREF,$e);
                 else
                     $this->Write(5,$e);
-            }
-            else
-            {
+            }else{
                 // Balise
                 if($e[0]=='/')
                     $this->CloseTag(strtoupper(substr($e,1)));
-                else
-                {
+                else{
                     // Extraction des attributs
                     $a2 = explode(' ',$e);
                     $tag = strtoupper(array_shift($a2));
                     $attr = array();
-                    foreach($a2 as $v)
-                    {
+                    foreach($a2 as $v){
                         if(preg_match('/([^=]*)=["\']?([^"\']*)/',$v,$a3))
                             $attr[strtoupper($a3[1])] = $a3[2];
                     }
@@ -98,8 +99,7 @@ function Footer(){
         // Modifie le style et sélectionne la police correspondante
         $this->$tag += ($enable ? 1 : -1);
         $style = '';
-        foreach(array('B', 'I', 'U') as $s)
-        {
+        foreach(array('B', 'I', 'U') as $s){
             if($this->$s>0)
                 $style .= $s;
         }
